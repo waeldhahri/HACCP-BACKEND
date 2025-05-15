e126c5f2404059c0326de118f5fd97c10dd33d27
waeldhahri <waeldhahri3@gmail.com>
2025-05-15 20:36:57 +0100

    ADD swagger-ui

--- src/main/java/com/example/haccpbackend/springSecurity/SecurityConfig.java
+++ src/main/java/com/example/haccpbackend/springSecurity/SecurityConfig.java
@@ -148,6 +148,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
